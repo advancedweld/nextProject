9aa71fed8bc66778481da5b4faf98532a0a9a998
xiangshangzhi <xiangshangzhi@163.com>
2024-06-28 13:50:09 +0800

    feat: 新增loading页

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+/*
+ * @Author: xiangshangzhi
+ * @Date: 2024-06-28 11:01:29
+ * @FilePath: \nextProject\src\app\loading.tsx
+ * @Description: loading页  https://nextjs.org/docs/app/building-your-application/routing/loading-ui-and-streaming
+ * loading页展示在layout页中，自动作为suspense的fallback
+ */
+
+export default function Loading() {
+  return <div>一级页面 loading...</div>;
+}
