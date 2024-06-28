d5c441d0c98bb94d2f5bed257058d78185c9cfad
xiangshangzhi <xiangshangzhi@163.com>
2024-06-28 10:35:35 +0800

    feat: 使用cssmodule

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 // 引入'@ant-design/nextjs-registry 解决样式问题
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import cx from 'classnames'
+import styles from './style.module.css'
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
+
 
 export const metadata: Metadata = {
   title: 'Xsz Next App',
@@ -18,8 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <div>根布局，layout共享ui</div>
+      <body className= {cx( inter.className,styles.layoutWrap )}>
+        <div className={styles.title}>根布局，layout共享ui</div>
         <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 /*
  * @Author: xiangshangzhi
  * @Date: 2024-02-23 10:57:42
- * @FilePath: \next-app\src\app\page.tsx
+ * @FilePath: \nextProject\src\app\page.tsx
  * @Description: next中使用antd 参考 https://ant-design.antgroup.com/docs/react/use-with-next-cn
  * https://github.com/ant-design/ant-design-examples/tree/main/examples/with-nextjs-app-router-inline-style
  */
 import Link from 'next/link';
 import { Button, Space } from 'antd';
+import styles from './style.module.css'
+
 export default function Page() {
   return (
-    <div>
+    <div className={styles.wrap}>
       <h1>Hello, Next.js!</h1>
       <Space>
         <Link href={'./dashboard'}>to dashboard</Link>
